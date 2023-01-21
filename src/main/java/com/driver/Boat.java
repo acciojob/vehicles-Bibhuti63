7a10c9100033942d537c84234d12adaa6977a828
Bibhuti <biswalbibhuti8@gmail.com>
2023-01-21 16:19:52 +0530

    second commit

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -4,6 +4,12 @@ public class Boat implements WaterVehicle{
 
     private String name;
     private int capacity;
+
+    public Boat(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+
     @Override
     public String getVehicleName() {
         return name;
